2b50c8f2504102c12a1bbb0ed1a427246cda29a4
Aldo <riecho14@outlook.com>
2022-06-09 14:35:03 +0700

    projects content updates

--- pages/projects.js
+++ pages/projects.js
@@ -3,74 +3,91 @@ import Section from '../components/section'
 import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 import { ProjectGridItem } from '../components/grid-item'
 
+import thumbReadmeIcons from '../public/images/projects/thumbReadmeIcons.png'
+import thumbDockerizedPygame from '../public/images/projects/thumbDockerizedPygame.png'
 import thumbDendamSiTikus from '../public/images/projects/thumbDendamSiTikus.png'
 import thumbOldPortfolio from '../public/images/projects/thumbOldPortfolio.png'
 import thumbGreenTrash from '../public/images/projects/thumbGreenTrash.png'
 
-const Projects = () => {
-  return (
-    <Layout title="Projects">
-      <Container>
+const Projects = () => (
+  <Layout title="Projects">
+    <Container>
+      <Heading as="h3" fontSize={20} mb={4}>
+        Projects
+      </Heading>
+
+      <SimpleGrid columns={[1, 1, 2]} gap={6}>
+        <Section>
+          <ProjectGridItem
+            id="greentrash"
+            title="Green Trash"
+            thumbnail={thumbGreenTrash}
+          >
+            A script that will graph your GitHub contributions green
+          </ProjectGridItem>
+        </Section>
+        <Section>
+          <ProjectGridItem
+            id="readmeicons"
+            title="Readme Icons"
+            thumbnail={thumbReadmeIcons}
+          >
+            List of icons that have been scaled and padded to work well when used in Github README
+          </ProjectGridItem>
+        </Section>
+      </SimpleGrid>
+
+      <Section delay={0.2}>
+        <Divider my={6} />
+
         <Heading as="h3" fontSize={20} mb={4}>
-          Projects
+          Collaborations
         </Heading>
+      </Section>
 
-        <SimpleGrid columns={[1, 1, 2]} gap={6}>
-          <Section>
-            <ProjectGridItem
-              id="greentrash"
-              title="Green Trash"
-              thumbnail={thumbGreenTrash}
-            >
-              A script that will graph your GitHub contributions green
-            </ProjectGridItem>
-          </Section>
-        </SimpleGrid>
-
-        <Section delay={0.2}>
-          <Divider my={6} />
-
-          <Heading as="h3" fontSize={20} mb={4}>
-            Collaborations
-          </Heading>
+      <SimpleGrid columns={[1, 1, 2]} gap={6}>
+        <Section delay={0.4}>
+          <ProjectGridItem
+            id="dendamsitikus"
+            title="Dendam Si Tikus"
+            thumbnail={thumbDendamSiTikus}
+          >
+            This project is a game development project using the pygame library
+          </ProjectGridItem>
         </Section>
-
-        <SimpleGrid columns={[1, 1, 2]} gap={6}>
-          <Section delay={0.4}>
-            <ProjectGridItem
-              id="dendamsitikus"
-              title="Dendam Si Tikus"
-              thumbnail={thumbDendamSiTikus}
-            >
-              This project is a game development project using the pygame
-              library
-            </ProjectGridItem>
-          </Section>
-        </SimpleGrid>
-
-        <Section delay={0.6}>
-          <Divider my={6} />
-
-          <Heading as="h3" fontSize={20} mb={4}>
-            Old Projects
-          </Heading>
+        <Section delay={0.4}>
+          <ProjectGridItem
+            id="dockerizedpygame"
+            title="Dockerized Pygame"
+            thumbnail={thumbDockerizedPygame}
+          >
+            Containerize Pygame with Docker
+          </ProjectGridItem>
         </Section>
+      </SimpleGrid>
 
-        <SimpleGrid columns={[1, 1, 2]} gap={6}>
-          <Section delay={0.8}>
-            <ProjectGridItem
-              id="oldportfolio"
-              title="Old Portfolio"
-              thumbnail={thumbOldPortfolio}
-            >
-              The first portfolio website I created using html, css, and
-              javascript
-            </ProjectGridItem>
-          </Section>
-        </SimpleGrid>
-      </Container>
-    </Layout>
-  )
-}
+      <Section delay={0.6}>
+        <Divider my={6} />
+
+        <Heading as="h3" fontSize={20} mb={4}>
+          Old Projects
+        </Heading>
+      </Section>
+
+      <SimpleGrid columns={[1, 1, 2]} gap={6}>
+        <Section delay={0.8}>
+          <ProjectGridItem
+            id="oldportfolio"
+            title="Old Portfolio"
+            thumbnail={thumbOldPortfolio}
+          >
+            The first portfolio website I created using html, css, and
+            javascript
+          </ProjectGridItem>
+        </Section>
+      </SimpleGrid>
+    </Container>
+  </Layout>
+)
 
 export default Projects
