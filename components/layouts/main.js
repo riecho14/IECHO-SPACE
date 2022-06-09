f15d76d6bc743418c94f3f584910a1a734e5e442
Aldo <riecho14@outlook.com>
2022-06-09 18:01:42 +0700

    add footer

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Navbar from '../navbar'
 import dynamic from 'next/dynamic'
 import VoxelIechoLoader from '../voxel-iecho-loader'
+import Footer from '../footer'
 import { Box, Container } from '@chakra-ui/react'
 
 const LazyIechoLoader = dynamic(() => import('../voxel-iecho'), {
@@ -22,6 +23,7 @@ const Main = ({ children, router }) => {
       <Container maxW="container.md" pt={14}>
         <LazyIechoLoader />
         {children}
+        <Footer />
       </Container>
     </Box>
   )
